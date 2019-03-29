5bdd53e773df61ff98c9c18ed2e3c80dd1a69de5
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2019-03-29 11:01:53 +0700

    add challenge

---
+++ neko/arrangeMe.js
@@ -0,0 +1,16 @@
+let number = 1234;
+let name = "mai";
+
+// Atur supaya mereka menjadi
+
+console.log(number); // Hasilnya adalah 1423
+console.log(name); // Hasilnya "mia"
+
+/*
+ * Hint
+ * Pertama adalah jadikan mereka array
+ * Lalu kalian harus me-loop-ing array tersebut dan ambil index pertama dan index terakhir lalu masukkan ke variable baru (berupa string)
+ * Setelah di masukkan ke variable, tentunya kita harus menghapus karakter yang sudah terpakai bukan ?
+ * Ada masalah ketika lengthnya ganjil ? tenang kalian bisa gunakan conditional disini
+ * Selamat berjuang!
+ */
---
+++ neko/changeUs.js
@@ -0,0 +1,16 @@
+let A = 10;
+let B = 5;
+
+// Silakan ketik sesuatu di bawah agar saat kita console.log maka A menjadi 5, dan B menjadi 10
+
+/*
+ * Hint
+ * Tambahkan variable baru yang berfungsi untuk menampung mereka
+ * Bayangkan sebuah segitiga A, B, dan C
+ * C adalah penampung nilai dari A atau B
+ * Lalu buat seolah olah mereka berputar nilainya, maksudnya A menjadi B dan B menjadi A dengan bantuan C
+ * Selamat Mengerjakan
+ */
+
+console.log(A); // Hasilnya harus 5
+console.log(B); // Hasilnya harus 10
