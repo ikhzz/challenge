025ded4787fd0702de70dbe195fee20158f60a95
ikhda <ikhdamuhammad@gmail.com>
2019-04-07 20:54:23 +0700

    add rps

--- neko/rps.js
+++ neko/rps.js
@@ -4,12 +4,13 @@
 Di berikan sebuah function yang menerima dua parameter, yakni inputan dari player dan inputan dari komputer,
 function akan mereturn player menang atau computer yang menang.
 
-1. Pertama tentukan dulu, jika scissor dan paper maka yang menang siapa
+1. Pertama pikirkan dulu, jika scissor dan paper maka yang menang siapa
 2. Lalu buat kondisi jika player scissor maka buatlah kondisi lagi di dalamnya yang akan membandingkan inputan computer dan player
     a. jika komputer menang maka output komputer menang
     b. jika player menang maka output player menang
     c. jika seri maka output seri
-3. Masukkan output ke variable result dan tampilkan
+3. Lakukan lagi untuk inputan player yang berupa rock atau paper
+4. Masukkan output ke variable result dan tampilkan
 .
 */
 
@@ -19,6 +20,6 @@ function rps(player, computer) {
   return result;
 }
 
-console.log(rps("scissor", "rock"));
-console.log(rps("rock", "scissor"));
-console.log(rps("paper", "rock"));
+console.log(rps("scissor", "rock")); // Player Win
+console.log(rps("rock", "scissor")); // Computer Win
+console.log(rps("paper", "paper")); // Draw
