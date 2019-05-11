1ae6657ecf54f75c0865ab435a95c3ae8fbe1807
ikhda <ikhdamuhammad@gmail.com>
2019-05-11 15:53:00 +0700

    add sum

---
+++ neko/sum.js
@@ -0,0 +1,10 @@
+/*
+Jumlahkan semua bilangan di dalam array
+*/
+
+function sum(array) {
+  // code goes here
+}
+
+const numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(sum(numbers)); // 45
